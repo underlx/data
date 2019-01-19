272b2276deb886a555ad60d9baa919c4b3c5bff5
brazanet <brazanet@hotmail.com>
2019-01-19 17:23:58 +0000

    Create achievements-pt-ml.sql

---
+++ achievements-pt-ml.sql
@@ -0,0 +1,104 @@
+INSERT INTO pp_achievement VALUES
+('548f5527-4b54-4646-a1a0-570328468d24', 'reach_level', '{"level": 2}', '', 200),
+('91d39484-65f4-44ee-9ea8-1ab45ff8244d', 'reach_level', '{"level": 10}', '', 200),
+('fc764c3f-7d35-486c-b0cc-d04021df5b8c', 'reach_level', '{"level": 20}', '', 200),
+('358db8e7-1abd-4672-a7e0-b90539b73da4', 'reach_level', '{"level": 30}', '', 200),
+('d9424992-ec89-4f95-b80e-a08bff6b9881', 'reach_level', '{"level": 40}', '', 200),
+('83f17b1c-f7e7-48f6-b904-c8adaa66bba0', 'reach_level', '{"level": 50}', '', 200),
+('d6763fd4-5c65-428a-97a2-35e7d99c49ed', 'reach_level', '{"level": 60}', '', 200),
+('6c97bcc1-5cf8-45c2-9599-8a161d5f99eb', 'reach_level', '{"level": 70}', '', 200),
+('30ecf819-d930-4a75-bd7b-bc6dadfa7892', 'reach_level', '{"level": 80}', '', 200),
+('86258e52-86c4-43f3-92bd-1e9b50f1cc72', 'reach_level', '{"level": 90}', '', 200),
+('6672ff56-2175-45dd-9b76-ab7119b6f335', 'reach_level', '{"level": 100}', '', 200),
+('3288ce03-f0dc-43e2-bd67-f21844bcea99', 'reach_level', '{"level": 110}', '', 200);
+
+INSERT INTO pp_achievement_name VALUES
+('548f5527-4b54-4646-a1a0-570328468d24', true,  'pt', 'Sempre a Subir', 'Subir um nível no PosPlay'),
+('548f5527-4b54-4646-a1a0-570328468d24', false, 'en', 'Going Up', 'Go up one level on PosPlay'),
+('91d39484-65f4-44ee-9ea8-1ab45ff8244d', true,  'pt', 'ML10', 'Chegar ao nível 10 no PosPlay'),
+('91d39484-65f4-44ee-9ea8-1ab45ff8244d', false, 'en', 'ML10', 'Reach level 10 on PosPlay'),
+('fc764c3f-7d35-486c-b0cc-d04021df5b8c', true,  'pt', 'ML20', 'Chegar ao nível 20 no PosPlay'),
+('fc764c3f-7d35-486c-b0cc-d04021df5b8c', false, 'en', 'ML20', 'Reach level 20 on PosPlay'),
+('358db8e7-1abd-4672-a7e0-b90539b73da4', true,  'pt', 'ML30', 'Chegar ao nível 30 no PosPlay'),
+('358db8e7-1abd-4672-a7e0-b90539b73da4', false, 'en', 'ML30', 'Reach level 30 on PosPlay'),
+('d9424992-ec89-4f95-b80e-a08bff6b9881', true,  'pt', 'ML40', 'Chegar ao nível 40 no PosPlay'),
+('d9424992-ec89-4f95-b80e-a08bff6b9881', false, 'en', 'ML40', 'Reach level 40 on PosPlay'),
+('83f17b1c-f7e7-48f6-b904-c8adaa66bba0', true,  'pt', 'ML50', 'Chegar ao nível 50 no PosPlay'),
+('83f17b1c-f7e7-48f6-b904-c8adaa66bba0', false, 'en', 'ML50', 'Reach level 50 on PosPlay'),
+('d6763fd4-5c65-428a-97a2-35e7d99c49ed', true,  'pt', 'ML60', 'Chegar ao nível 60 no PosPlay'),
+('d6763fd4-5c65-428a-97a2-35e7d99c49ed', false, 'en', 'ML60', 'Reach level 60 on PosPlay'),
+('6c97bcc1-5cf8-45c2-9599-8a161d5f99eb', true,  'pt', 'ML70', 'Chegar ao nível 70 no PosPlay'),
+('6c97bcc1-5cf8-45c2-9599-8a161d5f99eb', false, 'en', 'ML70', 'Reach level 70 on PosPlay'),
+('30ecf819-d930-4a75-bd7b-bc6dadfa7892', true,  'pt', 'ML80', 'Chegar ao nível 80 no PosPlay'),
+('30ecf819-d930-4a75-bd7b-bc6dadfa7892', false, 'en', 'ML80', 'Reach level 80 on PosPlay'),
+('86258e52-86c4-43f3-92bd-1e9b50f1cc72', true,  'pt', 'ML90', 'Chegar ao nível 90 no PosPlay'),
+('86258e52-86c4-43f3-92bd-1e9b50f1cc72', false, 'en', 'ML90', 'Reach level 90 on PosPlay'),
+('6672ff56-2175-45dd-9b76-ab7119b6f335', true,  'pt', '2059', 'Chegar ao nível 100 no PosPlay'),
+('6672ff56-2175-45dd-9b76-ab7119b6f335', false, 'en', '2059', 'Reach level 100 on PosPlay'),
+('3288ce03-f0dc-43e2-bd67-f21844bcea99', true,  'pt', 'Novo Jogo +', 'Chegar ao nível 110 no PosPlay'),
+('3288ce03-f0dc-43e2-bd67-f21844bcea99', false, 'en', 'New Game +', 'Reach level 110 on PosPlay');
+
+INSERT INTO pp_achievement VALUES
+('2ff66805-c20c-4eb8-868d-01693bdcdfc0', 'visit_stations', '{"stations": ["pt-ml-ae", "pt-ml-cs", "pt-ml-ec", "pt-ml-jz", "pt-ml-or", "pt-ml-rb", "pt-ml-re", "pt-ml-rm", "pt-ml-sp"]}', '', 200),
+('530dc21a-2bb5-4d25-a7ce-bc1bea265dfe', 'visit_stations', '{"stations": ["pt-ml-cs", "pt-ml-tp"]}', '', 200),
+('7627e75d-e18d-4c6b-82ae-f84046e5e77f', 'visit_stations', '{"stations": ["pt-ml-as", "pt-ml-cg", "pt-ml-cm", "pt-ml-cs", "pt-ml-jz", "pt-ml-mp", "pt-ml-od", "pt-ml-or", "pt-ml-po", "pt-ml-pe", "pt-ml-rb", "pt-ml-sr"]}', '', 200),
+('ccf3bb40-c5ad-489c-91c4-784ea596184c', 'visit_stations', '{"stations": ["pt-ml-ap"]}', '', 200),
+('b8d36eb1-fc04-43e7-a95f-f4092dc45cda', 'visit_stations', '{"stations": ["pt-ml-am", "pt-ml-av", "pt-ml-cg", "pt-ml-cp", "pt-ml-cs", "pt-ml-ec", "pt-ml-mm", "pt-ml-mp", "pt-ml-or", "pt-ml-pi", "pt-ml-rm", "pt-ml-sa", "pt-ml-ss", "pt-ml-te"]}', '', 200),
+('4082c303-b4c0-4094-bfe8-c1bfa89d44cd', 'visit_stations', '{"stations": ["pt-ml-ap", "pt-ml-af", "pt-ml-ca", "pt-ml-ch", "pt-ml-mm", "pt-ml-od", "pt-ml-ol", "pt-ml-or", "pt-ml-os", "pt-ml-po", "pt-ml-qc", "pt-ml-ra", "pt-ml-re", "pt-ml-te"]}', '', 200),
+
+
+
+
+
+
+
+
+
+
+
+
+('0f5f7558-b44e-49ba-ab39-07e281e15f0f', 'visit_stations', '{"stations": ["pt-ml-am", "pt-ml-ae", "pt-ml-al"]}', '', 200),
+('bc9c6850-32a9-4a1b-8389-d2fa898336e5', 'visit_stations', '{"stations": ["pt-ml-am", "pt-ml-ae", "pt-ml-al"], "singleTrip": true}', '', 200),
+('5f61c6d9-8ca1-421d-892c-e7f48d71979d', 'visit_stations', '{"stations": ["pt-ml-sp"]}', '', 50);
+
+INSERT INTO pp_achievement_name VALUES
+('2ff66805-c20c-4eb8-868d-01693bdcdfc0', true,  'pt', 'Platforma 9¾', 'Visitar todas as estações com ligação a uma estação ferroviária'),
+('2ff66805-c20c-4eb8-868d-01693bdcdfc0', false, 'en', 'Platform 9¾', 'Visit every station connected with a railway station'),
+('530dc21a-2bb5-4d25-a7ce-bc1bea265dfe', true,  'pt', 'Do Outro Lado do Rio', 'Visitar todas as estações com ligação a uma estação fluvial'),
+('530dc21a-2bb5-4d25-a7ce-bc1bea265dfe', false, 'en', 'Across the River', 'Visit every station connected with a ferry terminal'),
+('7627e75d-e18d-4c6b-82ae-f84046e5e77f', true,  'pt', '736', 'Visitar todas as estações com ligação a um terminal rodoviário'),
+('7627e75d-e18d-4c6b-82ae-f84046e5e77f', false, 'en', '736', 'Visit every station connected with a bus terminal'),
+('ccf3bb40-c5ad-489c-91c4-784ea596184c', true,  'pt', 'Chegadas', 'Visitar todas as estações com ligação a um aeroporto'),
+('ccf3bb40-c5ad-489c-91c4-784ea596184c', false, 'en', 'Arrivals', 'Visit every station connected with an airport'),
+('b8d36eb1-fc04-43e7-a95f-f4092dc45cda', true,  'pt', 'Ciclovia', 'Visitar todas as estações com ligação a uma estação de bicicletas GIRA'),
+('b8d36eb1-fc04-43e7-a95f-f4092dc45cda', false, 'en', 'Bike Lane', 'Visit every station connected with a GIRA bike station'),
+('4082c303-b4c0-4094-bfe8-c1bfa89d44cd', true,  'pt', 'LCPD', 'Visitar todas as estações próximas de uma esquadra de polícia'),
+('4082c303-b4c0-4094-bfe8-c1bfa89d44cd', false, 'en', 'LCPD', 'Visit every station close to a police station'),
+
+
+
+
+
+
+('0f5f7558-b44e-49ba-ab39-07e281e15f0f', true,  'pt', 'Teste AM, AE, AL', 'Visitar Alameda, Areeiro e Alvalade'),
+('0f5f7558-b44e-49ba-ab39-07e281e15f0f', false, 'en', 'AM, AE, AL test', 'Visit Alameda, Areeiro and Alvalade'),
+('bc9c6850-32a9-4a1b-8389-d2fa898336e5', true,  'pt', 'Teste AM, AE, AL numa viagem', 'Visitar Alameda, Areeiro e Alvalade numa só viagem'),
+('bc9c6850-32a9-4a1b-8389-d2fa898336e5', false, 'en', 'AM, AE, AL test in one trip', 'Visit Alameda, Areeiro and Alvalade numa só viagem'),
+('5f61c6d9-8ca1-421d-892c-e7f48d71979d', true,  'pt', 'Teste SP', 'Visitar Santa Apolónia'),
+('5f61c6d9-8ca1-421d-892c-e7f48d71979d', false, 'en', 'SP test', 'Visit Santa Apolónia');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
